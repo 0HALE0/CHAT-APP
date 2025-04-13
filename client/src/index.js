60a7ee790467c9f5a32a73d855dfcf34e65cb4ba
Hale <132852816+0HALE0@users.noreply.github.com>
2025-04-13 04:31:55 +0300

    Initial commit with client and server folders

---
+++ client/src/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App";
+
+// Find the root element in your HTML
+const rootElement = document.getElementById('root');
+
+// Create a root and render the App component
+const root = createRoot(rootElement);
+root.render(<App />);
